80d3a654c1caddc0a92627290fc712cedf3ca0d6
rahulsahay19 <rahulsahay19@gmail.com>
2016-01-30 23:19:05 +0530

    3rd Commit

--- MovieReview.Web/App_Start/BundleConfig.cs
+++ MovieReview.Web/App_Start/BundleConfig.cs
@@ -18,10 +18,11 @@ namespace MovieReview.Web
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/login.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-united.css",
                       "~/Content/site.css"));
         }
     }
